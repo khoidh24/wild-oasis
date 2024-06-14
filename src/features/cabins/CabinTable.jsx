e7fa5bf67a983a8413ea732b62d9ab0a1a8e204a
khoidh24 <zenorn24@gmail.com>
2024-06-14 14:30:17 +0700

    Add filter button

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -3,11 +3,21 @@ import CabinRow from './CabinRow'
 import { useLoadCabins } from './useLoadCabins'
 import Table from '../../ui/Table'
 import Menus from '../../ui/Menus'
+import { useSearchParams } from 'react-router-dom'
 
 const CabinTable = () => {
   const { isLoading, cabins } = useLoadCabins()
+  const [searchParams] = useSearchParams()
 
   if (isLoading) return <Spinner />
+
+  const filterValue = searchParams.get('discount') || 'all'
+  let filteredCabins
+  if (filterValue === 'all') filteredCabins = cabins
+  if (filterValue === 'no-discount')
+    filteredCabins = cabins.filter((cabin) => cabin.discount === 0)
+  if (filterValue === 'with-discount')
+    filteredCabins = cabins.filter((cabin) => cabin.discount > 0)
 
   return (
     <Menus>
@@ -22,7 +32,7 @@ const CabinTable = () => {
         </Table.Header>
 
         <Table.Body
-          data={cabins}
+          data={filteredCabins}
           render={(cabin) => <CabinRow key={cabin.id} cabin={cabin} />}
         />
       </Table>
